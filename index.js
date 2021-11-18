cd2f46e2eb12790033d5cf2cbb08ff930d0f94d9
Floaterest <56704092+Floaterest@users.noreply.github.com>
2021-11-18 13:39:56 -0500

    refactor(index.ts)

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ var o = size / 2;
 var a = o - r;
 var b = o + r;
 var r2 = r / 2;
-var m = o + r / 2;
-var MC = r * Math.sin(Math.PI / 3);
+var m = o + r2;
+var MC = r2 * Math.sqrt(3);
 var c = o - MC;
 var d = o + MC;
 var sqrt5 = Math.sqrt(5);
 var EN = r / sqrt5;
 var ey = o - EN;
 var ex = o + (r - EN) / 2;
-var AE = r * (sqrt5 - 1) / 2;
+var AE = r2 * (sqrt5 - 1);
 var fy = o - r * (sqrt5 + 1) / 4;
 var fx = o - r * Math.sqrt(10 - 2 * sqrt5) / 4;
-var g = o - r / 2;
+var g = o - r2;
 svg(size, size).append([
     circle(o, o, r).addClass('bound'),
     line(o, a, o, b),
@@ -32,8 +32,8 @@ svg(size, size).append([
     line(m, c, m, d),
     line(o, a, m, o),
     arc(o, o, r2, r2, 1, ex, ey),
-    arc(c, g, r, r, 1, fx, fy).addClass(['bound', 'accent']),
     arc(ex, ey, AE, AE, 1, fx, fy),
-    arc(o, o, r, r, 1, c, g), // arc 2
+    arc(o, o, r, r, 1, c, g),
+    arc(c, g, r, r, 1, fx, fy).addClass(['bound', 'accent']), // FG arc
 ]).appendTo(document.body);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -21,8 +21,8 @@ const a = o - r;
 const b = o + r;
 
 const r2 = r / 2;
-const m = o + r / 2;
-const MC = r * Math.sin(Math.PI / 3);
+const m = o + r2;
+const MC = r2 * Math.sqrt(3);
 const c = o - MC;
 const d = o + MC;
 
@@ -31,21 +31,21 @@ const EN = r / sqrt5;
 const ey = o - EN;
 const ex = o + (r - EN) / 2;
 
-const AE = r * (sqrt5 - 1) / 2;
+const AE = r2 * (sqrt5 - 1);
 const fy = o - r * (sqrt5 + 1) / 4;
 const fx = o - r * Math.sqrt(10 - 2 * sqrt5) / 4;
 
-const g = o - r / 2;
+const g = o - r2;
 
 svg(size, size).append([
-    circle(o, o, r).addClass('bound'),
-    line(o, a, o, b), // h line
-    line(a, o, b, o), // v line
-    arc(m, c, r, r, 0, m, d), // half arc
-    line(m, c, m, d), // half line
-    line(o, a, m, o), // oblique line
-    arc(o, o, r2, r2, 1, ex, ey), // small arc
-    arc(c, g, r, r, 1, fx, fy).addClass([ 'bound', 'accent' ]),
-    arc(ex, ey, AE, AE, 1, fx, fy), // arc 1
-    arc(o, o, r, r, 1, c, g), // arc 2
+    circle(o, o, r).addClass('bound'), // O circle
+    line(o, a, o, b), // x axis
+    line(a, o, b, o), // y axis
+    arc(m, c, r, r, 0, m, d), // CD arc
+    line(m, c, m, d), // CD line
+    line(o, a, m, o), // AM line
+    arc(o, o, r2, r2, 1, ex, ey), // OE arc
+    arc(ex, ey, AE, AE, 1, fx, fy), // EF arc
+    arc(o, o, r, r, 1, c, g), // OG arc
+    arc(c, g, r, r, 1, fx, fy).addClass([ 'bound', 'accent' ]), // FG arc
 ]).appendTo(document.body);
