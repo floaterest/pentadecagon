5365bd08f37c28c4e1d0805933eb7985baae7f6b
Floaterest <56704092+Floaterest@users.noreply.github.com>
2021-11-22 09:11:55 -0500

    feat(App,Complete): dynamic radius and stroke-width

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
     import Complete from './Complete.svelte';
+
+    let option = {
+        radius: 500,
+        swidth: 5,
+        stroke: 'white',
+        accent: '#39c5bb',
+        background: 'black',
+    };
 </script>
 
 <main>
-    <Complete r={500} swidth={10} scolor="white" background="black" accent="#39c5bb"/>
+    <div>
+        <input type="number" bind:value={option.radius}>
+        <input type="number" bind:value={option.swidth}>
+    </div>
+    <Complete {...option}/>
 </main>
