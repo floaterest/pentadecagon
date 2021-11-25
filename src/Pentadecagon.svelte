f419595baa4623a09901fe68fefc25128bd26caa
Floaterest <56704092+Floaterest@users.noreply.github.com>
2021-11-25 09:23:35 -0500

    refactor(Pent)

--- src/Pentadecagon.svelte
+++ src/Pentadecagon.svelte
@@ -20,7 +20,7 @@
     $: r2 = r / 2;
 
     // coordinates
-    $: c = ((o: number, r: number, r2: number) => {
+    $: cds = ((o: number, r: number, r2: number) => {
         let sqrt5 = Math.sqrt(5);
         let EN = r / sqrt5;
         let MC = r2 * Math.sqrt(3);
@@ -67,7 +67,7 @@
             x3: o - x(r + c, y3), y3: o - y3, r3: r - s,
             x4: o - x(r - c, y4), y4: o - y4, r4: r - c,
         };
-    })(c.AF, r, r2, swidth / 2, cwidth / 2);
+    })(cds.AF, r, r2, swidth / 2, cwidth / 2);
 
     $: square = `m0 ${-fs} l${fs} 0 l0 ${fs}`;
 </script>
@@ -77,28 +77,28 @@
      style="background-color:{background};">
     <circle stroke-width={cwidth} cx={o} cy={o} r={r}></circle>
     <!-- x axis -->
-    <line x1={o} y1={c.ay} x2={o} y2={c.bx}></line>
+    <line x1={o} y1={cds.ay} x2={o} y2={cds.bx}></line>
     <!-- y axis -->
-    <line x1={c.ay} y1={o} x2={c.bx} y2={o}></line>
+    <line x1={cds.ay} y1={o} x2={cds.bx} y2={o}></line>
     <!-- CD line -->
-    <line x1={c.mx} y1={c.cy} x2={c.mx} y2={c.dy}></line>
+    <line x1={cds.mx} y1={cds.cy} x2={cds.mx} y2={cds.dy}></line>
     <!-- AM line -->
-    <line x1={o} y1={c.ay} x2={c.mx} y2={o}></line>
+    <line x1={o} y1={cds.ay} x2={cds.mx} y2={o}></line>
     <!-- CD arc -->
-    <path d="M{c.mx} {c.dy} A{r} {r} 0 0 1 {c.mx} {c.cy}"></path>
+    <path d="M{cds.mx} {cds.dy} A{r} {r} 0 0 1 {cds.mx} {cds.cy}"></path>
     <!-- OE arc -->
-    <path d="M{o} {o} A{r2} {r2} 0 0 1 {c.nx} {c.ey}"></path>
+    <path d="M{o} {o} A{r2} {r2} 0 0 1 {cds.nx} {cds.ey}"></path>
     <!-- EF arc -->
-    <path d="M{c.nx} {c.ey} A{c.AF} {c.AF} 0 0 1 {c.fx} {c.fy}"></path>
+    <path d="M{cds.nx} {cds.ey} A{cds.AF} {cds.AF} 0 0 1 {cds.fx} {cds.fy}"></path>
     <!-- OG arc -->
-    <path d="M{o} {o} A{r} {r} 0 0 1 {c.cy} {c.gy}"></path>
+    <path d="M{o} {o} A{r} {r} 0 0 1 {cds.cy} {cds.gy}"></path>
 
     {#if math}
         <!-- auxiliary lines at CB EN GP GO AF -->
-        <path d="M{c.mx} {c.cy} L{c.bx} {o} M{c.nx} {c.ey} V{o} M{c.cy} {o} v{-r2} L{o} {o} m0 {-r} L{c.fx} {c.fy}"
+        <path d="M{cds.mx} {cds.cy} L{cds.bx} {o} M{cds.nx} {cds.ey} V{o} M{cds.cy} {o} v{-r2} L{o} {o} m0 {-r} L{cds.fx} {cds.fy}"
               stroke-dasharray={r / 50}></path>
         <!-- right angles at M N O P -->
-        <path d="M{c.mx} {o} {square} M{c.nx} {o} {square} M{o} {o} {square} M{c.cy} {o} {square}"></path>
+        <path d="M{cds.mx} {o} {square} M{cds.nx} {o} {square} M{o} {o} {square} M{cds.cy} {o} {square}"></path>
     {:else}
         <!-- FG arc (accent) -->
         <path d="M{acc.x1} {acc.y1} A{acc.r1} {acc.r1} 0 0 1 {acc.x2} {acc.y2} A{acc.r2} {acc.r2} 0 0 0 {acc.x3} {acc.y3} A{acc.r3} {acc.r3} 0 0 0 {acc.x4} {acc.y4} A{acc.r4} {acc.r4} 0 0 1 {acc.x1} {acc.y1}"
@@ -108,18 +108,18 @@
 {#if math}
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {size} {size}"
          width={size} height={size} fill={stroke} font-size={fs}>
-        <text x={o+fs} y={c.ay+fs}>A(o,ay)</text>
-        <text x={c.bx-fs*5} y={o-fs2}>B(bx,o)</text>
-        <text x={c.mx+fs2} y={c.cy-fs2}>C(mx,cy)</text>
-        <text x={c.mx} y={c.dy+fs}>D(mx,dy)</text>
-        <text x={c.nx+fs2} y={c.ey-fs2}>E(nx,ey)</text>
-        <text x={c.fx+fs2} y={c.fy+fs2}>F(fx,fy)</text>
-        <text x={c.cy+fs2} y={c.gy}>G(cy,gy)</text>
+        <text x={o+fs} y={cds.ay+fs}>A(o,ay)</text>
+        <text x={cds.bx-fs*5} y={o-fs2}>B(bx,o)</text>
+        <text x={cds.mx+fs2} y={cds.cy-fs2}>C(mx,cy)</text>
+        <text x={cds.mx} y={cds.dy+fs}>D(mx,dy)</text>
+        <text x={cds.nx+fs2} y={cds.ey-fs2}>E(nx,ey)</text>
+        <text x={cds.fx+fs2} y={cds.fy+fs2}>F(fx,fy)</text>
+        <text x={cds.cy+fs2} y={cds.gy}>G(cy,gy)</text>
 
-        <text x={c.mx+fs2} y={o+fs}>M(mx,o)</text>
-        <text x={c.nx+fs2} y={o+fs}>N(nx,o)</text>
+        <text x={cds.mx+fs2} y={o+fs}>M(mx,o)</text>
+        <text x={cds.nx+fs2} y={o+fs}>N(nx,o)</text>
         <text x={o+fs2} y={o+fs}>O(o,o)</text>
-        <text x={c.cy+fs2} y={o+fs}>P(cy,o)</text>
+        <text x={cds.cy+fs2} y={o+fs}>P(cy,o)</text>
     </svg>
     <style>
         svg{
