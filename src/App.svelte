6f88b0f9a9ff3faba50c239a3b2c41012fa3947d
floaterest <56704092+floaterest@users.noreply.github.com>
2021-11-30 11:02:23 -0500

    feat(App): add play button

--- src/App.svelte
+++ src/App.svelte
@@ -4,16 +4,25 @@
     let option = {
         r: 500,
         swidth: 20,
-        cwidth: 20,
+        cwidth: 40,
         stroke: 'white',
         accent: '#39c5bb',
         background: 'black',
     };
     let math = false;
+    let visible = true;
+
+    function handleClick(){
+        visible = false;
+        setTimeout(() => {
+            visible = true;
+        }, 1);
+    }
 </script>
 
 <main>
     <div>
+        <button on:click={handleClick}>play</button>
         <input type="number" bind:value={option.r}>
         <input type="number" bind:value={option.swidth} disabled={math?"disabled":""}>
         <input type="number" bind:value={option.cwidth} disabled={math?"disabled":""}>
@@ -22,9 +31,11 @@
         <input type="text" bind:value={option.background}>
         <input type="checkbox" bind:checked={math}>
     </div>
-    {#if math}
-        <Pentadecagon {...option} swidth={1} cwidth={1} math={math}/>
-    {:else}
-        <Pentadecagon {...option}/>
+    {#if visible}
+        {#if math}
+            <Pentadecagon {...option} swidth={1} cwidth={1} math={math}/>
+        {:else}
+            <Pentadecagon {...option}/>
+        {/if}
     {/if}
 </main>
