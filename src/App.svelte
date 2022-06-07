7ac4a0a601eabd1ef4c76eeb784f1f6b61237276
Floaterest <floaterest@outlook.jp>
2022-06-07 18:47:35 -0400

    feat(*.svelte) no math

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
     import Pentadecagon from './Pentadecagon.svelte';
-    import Annotations from './Annotations.svelte';
 
     import { option, createOption } from './stores';
     import { transitions, init } from './constants';
     import type { Transition } from './Interfaces';
 
-    let math = false;
     let visible = true;
     let transition = transitions[0];
 
@@ -29,27 +27,18 @@
 <main>
     <div>
         <input type="number" bind:value={opt.r}>
-        <input type="number" bind:value={opt.swidth} disabled={math?"disabled":""}>
-        <input type="number" bind:value={opt.cwidth} disabled={math?"disabled":""}>
+        <input type="number" bind:value={opt.swidth}>
+        <input type="number" bind:value={opt.cwidth}>
         <input type="text" bind:value={opt.stroke}>
         <input type="text" bind:value={opt.accent}>
         <input type="text" bind:value={opt.background}>
-        <input type="checkbox" on:click={rerender} bind:checked={math}>
     </div>
     <div>
         {#each transitions as tr,i}
-            <button on:click={()=>chtr(tr)} disabled={math?"disabled":""}>{i}</button>
+            <button on:click={()=>chtr(tr)}>{i}</button>
         {/each}
     </div>
     {#if visible}
-        <Pentadecagon {math} {transition}/>
-        {#if math}
-            <Annotations/>
-            <style>
-                svg{
-                    position: absolute;
-                }
-            </style>
-        {/if}
+        <Pentadecagon {transition}/>
     {/if}
 </main>
